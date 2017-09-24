4cb743ce81e9f66e812816039a3a4e9aa039c19a
chazmcgrill <charlietaylordev@gmail.com>
2017-09-24 14:16:09 +0100

    Largest of Four / Title Case
    Created algorithms for the largest of four numbers in a array and title
    casing strings.

--- longest-word.js
+++ longest-word.js
@@ -1,14 +1,14 @@
+/* Splits string into seperate words. Compares
+each word with the first word and returns length
+of the longest word */
+
 function findLongestWord(str) {
-  
   var arr = str.split(' '),
       result = arr[0];
-
-  arr.forEach(function(item) {
-    if (item.length > result.length) result = item;
+  arr.forEach(function(word) {
+    if (word.length > result.length) result = word;
   });
-
   return result.length;
-
 }
 
 console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
