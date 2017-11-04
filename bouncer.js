ff29b88402baed9fa17b8876578e3f1986fd0643
chazmcgrill <charlietaylordev@gmail.com>
2017-11-04 14:32:11 +0000

    Tidied Bouncer Algorithm
    consolidated bouncer algo into a return of the filtered array rather than storing in variables.

--- bouncer.js
+++ bouncer.js
@@ -1,13 +1,12 @@
-/* Creates a filtered variable and uses the filter 
+/* Creates a filtered variable and uses the filter
 method to remove falsy values. Only requires single
 return statement with the element as filter removes
 false values. */
 
 function bouncer(arr) {
-  var filtered = arr.filter(function(element) {
+  return arr.filter(function(element) {
     return element;
   });
-  return filtered;
 }
 
 console.log(bouncer([7, "ate", "", false, 9]));
