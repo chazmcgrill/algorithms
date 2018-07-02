0f3e8cb642fc2f9dae7659e1a0bae9bfbecad672
chazmcgrill <charlietaylordev@gmail.com>
2018-07-02 11:20:10 +0100

    Improved Franken Splice

--- franken-splice.js
+++ franken-splice.js
@@ -1,9 +1,6 @@
 function frankenSplice(arr1, arr2, n) {
   let result = arr2.slice();
-  for (let i = 0, k = arr1.length; i < k; i++) {
-    result.splice(n, 0, arr1[i]);
-    n++;
-  }
+  result.splice(n, 0, ...arr1);
   return result;
 }
 
