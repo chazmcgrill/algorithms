41527148c8a65f6693cddc6348d1fe8660c18745
chazmcgrill <charlietaylorcoder@gmail.com>
2021-12-24 10:26:05 +0000

    ci: sorts out prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,23 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-    ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier'
-    ],
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    plugins: ['@typescript-eslint'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    rules: {
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error', { variables: false }],
+        'spaced-comment': 'error',
+        'no-inline-comments': 'error',
+        'no-console': 'error',
+        'no-warning-comments': ['warn', { terms: ['@todo', 'todo', 'fixme'], location: 'start' }],
+        '@typescript-eslint/lines-between-class-members': [
+            'error',
+            'always',
+            { exceptAfterOverload: true, exceptAfterSingleLine: true },
+        ],
+        '@typescript-eslint/no-floating-promises': 'error',
+    },
 };
