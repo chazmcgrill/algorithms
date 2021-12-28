86bd5ed0090bfd8fdd2169c7c4db5426baaf7be8
chazmcgrill <charlietaylorcoder@gmail.com>
2021-12-28 11:35:07 +0000

    refactor: moves sorting algos into own folder

--- src/algorithms/sorting/bubbleSort.ts
+++ src/algorithms/sorting/bubbleSort.ts
@@ -1,15 +1,3 @@
-export function insertionSort<T>(arr: T[]): T[] {
-    for (let i = 0; i < arr.length; i++) {
-        const val = arr[i];
-        let j;
-        for (j = i - 1; j > -1 && arr[j] > val; j--) {
-            arr[j + 1] = arr[j];
-        }
-        arr[j + 1] = val;
-    }
-    return arr;
-}
-
 export function bubbleSort<T>(arr: T[]): T[] {
     let swapCount;
     do {
