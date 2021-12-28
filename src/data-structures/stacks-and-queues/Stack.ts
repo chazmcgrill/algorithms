c38716f0a2759dc62a0825e5b748773da3de58cf
chazmcgrill <charlietaylorcoder@gmail.com>
2021-12-28 17:48:14 +0000

    docs: adds code documentation for stack

--- src/data-structures/stacks-and-queues/Stack.ts
+++ src/data-structures/stacks-and-queues/Stack.ts
@@ -1,5 +1,6 @@
 import { StackImplementation } from './types';
 
+/** Stack data structure last in first out (LIFO) */
 export class Stack<T> implements StackImplementation<T> {
     private stack: T[];
     private stackLimit: number;
@@ -13,24 +14,29 @@ export class Stack<T> implements StackImplementation<T> {
         return this.stack.length - 1;
     }
 
+    /** Tells us if the stack is empty */
     get isEmpty() {
         return this.stack.length === 0;
     }
 
+    /** Tells us if the stack is full */
     get isFull() {
         return this.stack.length === this.stackLimit;
     }
 
+    /** Adds item to the top of the data stack */
     push(data: T) {
         if (this.isFull) throw new Error('Stack is full');
         return this.stack.push(data);
     }
 
+    /** Pops the top item off the stack */
     pop() {
         if (this.isEmpty) throw new Error('Stack is empty');
         return this.stack.pop();
     }
 
+    /** Looks at the item at the top of the stack without popping */
     peek() {
         if (this.isEmpty) throw new Error('Stack is empty');
         const stackTopIndex = this.getStackTopIndex;
